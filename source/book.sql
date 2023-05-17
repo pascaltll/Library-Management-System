9ed4d0cccad31e4476be25a75c6cc8b6eda4541b
Phan Ba Duc <47948344+HeoKiu@users.noreply.github.com>
2023-05-18 01:50:10 +0300

    Update book.sql

--- source/book.sql
+++ source/book.sql
@@ -1,8 +1,3 @@
--- MySQL dump 10.13  Distrib 5.7.21, for Win64 (x86_64)
---
--- Host: localhost    Database: book
--- ------------------------------------------------------
--- Server version	5.7.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -565,4 +560,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-02 12:03:22
+
