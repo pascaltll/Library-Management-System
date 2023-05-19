c9bb110162ac2a84c72e52bdb15b8ade999d511b
Phan Ba Duc <47948344+HeoKiu@users.noreply.github.com>
2023-05-19 03:13:32 +0300

    Update admin.py

--- library/admin.py
+++ library/admin.py
@@ -1,13 +1,4 @@
 from django.contrib import admin
+from library.models import AuthUser, Books, Libraries, Admins, Loans, Reserves, Comments, Storages
 
-# Register your models here.
-from library.models import AuthUser,Books,Libraries,Admins,Loans,Reserves,Comments,Storages
-
-admin.site.register(AuthUser)
-admin.site.register(Books)
-admin.site.register(Libraries)
-admin.site.register(Admins)
-admin.site.register(Loans)
-admin.site.register(Reserves)
-admin.site.register(Comments)
-admin.site.register(Storages)
+admin.site.register([AuthUser, Books, Libraries, Admins, Loans, Reserves, Comments, Storages])
