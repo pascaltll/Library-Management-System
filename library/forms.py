c64e181ef8c7e3cfca47a8753ea6dd3471e783d7
Phan Ba Duc <47948344+HeoKiu@users.noreply.github.com>
2023-05-19 03:19:51 +0300

    Update forms.py

--- library/forms.py
+++ library/forms.py
@@ -1,28 +1,10 @@
 from django import forms
-
-from .models import AuthUser,Books,Storages,Comments,Reserves
+from .models import AuthUser, Books, Comments
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = AuthUser
-        # fields = [ 'id', 'password','last_login','is_superuser','username',
-        #           'first_name', 'last_name','email','is_staff','is_active','date_joined']
-        fields = [  'first_name', 'last_name','email' ]
-
-# class BookForm(forms.ModelForm):
-#     class Meta:
-#         model = Books
-#         fields = [ 'isbn', 'title', 'authors', 'publisher', 'price' ]
-
-# class StorageForm(forms.ModelForm):
-#     class Meta:
-#         model = Storages
-#         fields = [ 'stono', 'isbn', 'lno']
-
-# class AddBookForm(forms.ModelForm):
-#     class Meta:
-#         model = Books, Storages
-#         fields = [ 'isbn', 'title', 'authors', 'publisher', 'price', 'lno']
+        fields = ['first_name', 'last_name', 'email']
 
 class AddBookForm(forms.Form):
     isbn = forms.CharField(max_length=50)
@@ -30,28 +12,27 @@ class AddBookForm(forms.Form):
     authors = forms.CharField(max_length=50)
     publisher = forms.CharField(max_length=50)
     price = forms.FloatField()
-    lno = forms.ChoiceField(choices=(('1', 'Lee Shau Kee Library'),
-                                         ('2', 'Liberal Arts Library'),
-                                         ('3', 'Science Library'),
-                                         ('4', 'Zhangjiang Library')))
-    #lno = forms.IntegerField(max_value=4,min_value=1)
-
-    lno.widget.attrs.update({'class':'dropdown-trigger waves-effect'})
+    lno = forms.ChoiceField(choices=(
+        ('1', 'Ziuzino Library'),
+        ('2', 'Dolgoprudny Library I'),
+        ('3', 'Zhukovski Library'),
+        ('4', 'Dolgoprudny Library II')
+    ))
+    lno.widget.attrs.update({'class': 'dropdown-trigger waves-effect'})
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comments
-        fields = [ 'text']
+        fields = ['text']
 
 class ReserveForm(forms.Form):
-
-    lno = forms.ChoiceField(choices=(('1', 'Lee Shau Kee Library'),
-                                         ('2', 'Liberal Arts Library'),
-                                         ('3', 'Science Library'),
-                                         ('4', 'Zhangjiang Library')))
-    #lno = forms.IntegerField(max_value=4,min_value=1)
-
-    lno.widget.attrs.update({'class':'dropdown-trigger waves-effect'})
+    lno = forms.ChoiceField(choices=(
+        ('1', 'Ziuzino Library'),
+        ('2', 'Dolgoprudny Library I'),
+        ('3', 'Zhukovski Library'),
+        ('4', 'Dolgoprudny Library II')
+    ))
+    lno.widget.attrs.update({'class': 'dropdown-trigger waves-effect'})
 
 class SearchBookForm(forms.Form):
-    text=forms.CharField(max_length=50, required=False)
+    text = forms.CharField(max_length=50, required=False)
