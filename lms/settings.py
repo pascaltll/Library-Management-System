fac9b0b9f23483770021a154e6ee5f930c73944c
Phan Ba Duc <47948344+HeoKiu@users.noreply.github.com>
2023-05-19 03:46:28 +0300

    Update settings.py

--- lms/settings.py
+++ lms/settings.py
@@ -88,8 +88,6 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -112,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -121,8 +118,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
 
 STATIC_URL = '/static/'
 
